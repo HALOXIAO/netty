9d03f0e2e05b965a5cb560ac9dff7034fa6b35d2
HALOXIAO <584714929@qq.com>
2021-07-01 21:53:11 +0800

    temp

--- example/src/main/java/io/netty/example/demo/http/ClientHandler.java
+++ example/src/main/java/io/netty/example/demo/http/ClientHandler.java
@@ -1,18 +1,26 @@
 package io.netty.example.demo.http;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.util.CharsetUtil;
 
 /**
  * @author HALOXIAO
  * @since 2021/6/30
  */
-public class ClientHandler extends ChannelInboundHandlerAdapter {
+public class ClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
 
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println(msg.toString());
+        }
 
 
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("this is lient", CharsetUtil.UTF_8));
+    }
 }
